8d117ce1fd7596be89f394be4e8c5f36cc332636
qinghanz <qinghan.zhang@mail.mcgill.ca>
2022-07-05 17:25:48 -0400

    changed server link

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ export default class App extends Component {
     let cart = localStorage.getItem("cart");
 
     const products = await axios.get(
-      "https://my-json-server.typicode.com/qinghanz/jsonserver/products"
+      "https://my-json-server.typicode.com/qinghanz/e-commerce/db.json/products"
     );
 
     cart = cart ? JSON.parse(cart) : {};
